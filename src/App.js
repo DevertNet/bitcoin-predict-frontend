368993aeea3b85a11e5ba190fbf2e43eacdadf27
Leon Weickert <leon@Air-von-Leon.lan>
2023-04-15 10:35:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import { Chart } from "chart.js/auto";
+import axios from "axios";
 
 function App() {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    Promise.all([
+      axios.get(
+        "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=365"
+      ),
+      axios.get("https://bitcoin-predict-backend.ddev.site/api/v1/list"),
+    ])
+      .then((responses) => {
+        const coingeckoPrices = responses[0].data.prices.map(
+          (price) => price[1]
+        );
+        const coingeckoDates = responses[0].data.prices.map((price) =>
+          new Date(price[0]).toLocaleDateString("de-DE", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+          })
+        );
+        const predictPrices = [];
+        const predictDates = [];
+
+        const predictDataRaw = responses[1].data;
+        const predictData = coingeckoDates.map((date) => {
+          if (predictDataRaw[date]) {
+            return predictDataRaw[date];
+          } else {
+            return 0;
+          }
+        });
+
+        const chart = new Chart(chartRef.current, {
+          type: "bar",
+          data: {
+            labels: coingeckoDates,
+            datasets: [
+              {
+                type: "line",
+                label: "Bitcoin Price (Coingecko)",
+                data: coingeckoPrices,
+                borderColor: "rgb(255, 99, 132)",
+                backgroundColor: "rgba(255, 99, 132, 0.2)",
+                yAxisID: "price",
+              },
+              {
+                type: "bar",
+                label: "Prediction",
+                data: predictData,
+                backgroundColor: "rgb(54, 162, 235)",
+                yAxisID: "prediction",
+              },
+            ],
+          },
+          options: {
+            scales: {
+              price: {
+                type: "linear",
+                display: true,
+                position: "left",
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+              prediction: {
+                type: "linear",
+                display: true,
+                position: "right",
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            },
+          },
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <canvas ref={chartRef} />
     </div>
   );
 }
