2ee9bfbc540ebaf7e156847bcff34814f37ea94b
Leon Weickert <leon@Air-von-Leon.lan>
2023-04-20 13:02:23 +0200

    feat: implement loading indicator; fix useEffect twice bug by remove strictmode

--- src/components/AnalyseTable.js
+++ src/components/AnalyseTable.js
@@ -2,51 +2,58 @@ import React, { useState, useEffect } from "react";
 
 function AnalyseTable() {
   const [matrix, setMatrix] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       const response = await fetch(
         `${process.env.REACT_APP_BACKEND_API_URL}/analyse`
       );
       const matrix = await response.json();
       setMatrix(matrix);
+      setIsLoading(false);
     }
     fetchData();
   }, []);
 
   return (
-    <div style={{ display: "flex", gap: "30px" }}>
-      {matrix.map((instance, instanceIndex) => (
-        <table border="1">
-          <thead>
-            <tr>
-              <th>date</th>
-              <th>price</th>
-              <th>score</th>
-              <th>advise</th>
-              <th>fiatBalanceAfterAdvise</th>
-              <th>bitcoinBalanceAfterAdvise</th>
-            </tr>
-          </thead>
-          <tbody>
-            {instance.map((row) => (
-              <tr
-                key={[
-                  instanceIndex,
-                  new Date(row.date.date).toLocaleDateString(),
-                ].join("-")}
-              >
-                <td>{new Date(row.date.date).toLocaleDateString()}</td>
-                <td>{row.price.toFixed(2)}</td>
-                <td>{row.score}</td>
-                <td>{row.advise}</td>
-                <td>{row.fiatBalanceAfterAdvise.toFixed(2)}</td>
-                <td>{row.bitcoinBalanceAfterAdvise.toFixed(4)}</td>
+    <div>
+      <h2>Result of /analyse Endpoint</h2>
+      {isLoading && <p>Loading...</p>}
+      <div style={{ display: "flex", gap: "30px" }}>
+        {matrix.map((instance, instanceIndex) => (
+          <table border="1">
+            <thead>
+              <tr>
+                <th>date</th>
+                <th>price</th>
+                <th>score</th>
+                <th>advise</th>
+                <th>fiatBalanceAfterAdvise</th>
+                <th>bitcoinBalanceAfterAdvise</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      ))}
+            </thead>
+            <tbody>
+              {instance.map((row) => (
+                <tr
+                  key={[
+                    instanceIndex,
+                    new Date(row.date.date).toLocaleDateString(),
+                  ].join("-")}
+                >
+                  <td>{new Date(row.date.date).toLocaleDateString()}</td>
+                  <td>{row.price.toFixed(2)}</td>
+                  <td>{row.score}</td>
+                  <td>{row.advise}</td>
+                  <td>{row.fiatBalanceAfterAdvise.toFixed(2)}</td>
+                  <td>{row.bitcoinBalanceAfterAdvise.toFixed(4)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ))}
+      </div>
     </div>
   );
 }
